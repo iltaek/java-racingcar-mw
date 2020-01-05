8db8faeb96566a2b03dffe039f58e9e2a5580198
joon36 <kwjoon7@gmail.com>
2020-01-06 00:34:33 +0900

    결과 출력 클래스 리팩토링

--- src/main/java/ResultPrinter.java
+++ src/main/java/ResultPrinter.java
@@ -7,17 +7,20 @@ public class ResultPrinter {
         System.out.print("\n실행 결과");
 
         IntStream.range(0, racingGame.getRound())
-            .forEach(i -> {
-                racingGame.getPlayers().forEach(c-> printEachRoundScore(c,c.getScores().get(i)));
-                System.out.println();
-            });
-
+            .forEach(i -> printAllPlayerEachRound(racingGame, i));
         printWinner(racingGame);
     }
 
-    private static void printEachRoundScore(Car car, int score) {
+    private static void printAllPlayerEachRound(RacingGame racingGame, int round) {
+        racingGame.getPlayers().forEach(c -> printEachRoundScore(c, round));
+        System.out.println();
+    }
+
+    private static void printEachRoundScore(Car car, int round) {
         System.out.print("\n" + car.getName() + " : ");
-        IntStream.range(0, score).forEach(i -> System.out.print("-"));
+
+        int scoreOfSpecificRound = car.getScores().get(round);
+        IntStream.range(0, scoreOfSpecificRound).forEach(i -> System.out.print("-"));
     }
 
     private static void printWinner(RacingGame racingGame) {
