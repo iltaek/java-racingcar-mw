fca01cc2839ac3fe975cb482c7cedc8ea1bd7ca2
joon36 <kwjoon7@gmail.com>
2020-01-05 17:30:44 +0900

    2회 이상의 round를 진행

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,18 +1,28 @@
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 public class Car {
 
     final String name;
-    public boolean record;
+    final List<Boolean> record;
 
     public Car(String name) {
         this.name = name;
+        record = new LinkedList<>();
     }
 
-    public int go() {
+    public int goEachRound() {
         Random random = new Random();
         int randomNumber = random.nextInt(10);
-        record = randomNumber >= 4;
+        record.add(randomNumber >= 4);
         return randomNumber;
+    }
+
+    public List<Boolean> goEveryRound(int round) {
+        for(int i =0; i<round ; i++){
+            goEachRound();
+        }
+        return record;
     }
 }
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,18 +1,33 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
 
     @Test
-    @DisplayName("car가 랜덤 값을 받아서 4이상이면 true, 그렇지않으면 false를 반환하는지 테스트")
-    public void goTest(){
+    @DisplayName("car가 go를 수행할 round수를 받고, 수행 결과를 List로 반환하는 테스트")
+    public void goEveryRoundTest(){
+        //given
+        Car car = new Car("CarA");
+
+        //when
+        List<Boolean> record = car.goEveryRound(3);
+
+        //then
+        assertThat(record.size()).isEqualTo(3);
+
+    }
+
+    @Test
+    @DisplayName("car가 랜덤 값을 받아서 4이상이면 true, 그렇지않으면 false를 반환하는 테스트")
+    public void goEachRoundTest(){
         //given
         Car car = new Car("CarA");
         //when
-        int randomNumber = car.go();
+        int randomNumber = car.goEachRound();
         //then
-        assertThat(randomNumber >= 4).isEqualTo(car.record);
+        assertThat(randomNumber >= 4).isEqualTo(car.record.get(0));
     }
 }
