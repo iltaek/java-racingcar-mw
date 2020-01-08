0c4e36a944fecbec11dc3045bc3e9e2910a085f3
skyrainyday <lcc87@hanmail.net>
2020-01-08 20:18:07 +0900

    feat(controller) : Add class - RacingCarGameLauncher

---
+++ src/main/java/RacingCarGameLauncher.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class RacingCarGameLauncher {
+    public void launch(List<Car> carList, int numberOfExecution) {
+        RacingCarGame racingCarGame = new RacingCarGame();
+        RacingCarGameResult racingCarGameResult = new RacingCarGameResult();
+
+        for (int i = 0; i < numberOfExecution; i++) {
+            racingCarGame.playSingleGame(carList);
+            racingCarGameResult.prepareIntermediateResult(carList);
+        }
+
+        List<Car> winnersList = getWinnersList(carList);
+        racingCarGameResult.prepareFinalResult(winnersList);
+
+        String result = racingCarGameResult.getResult();
+        System.out.println(result);
+    }
+
+    public List<Car> getWinnersList(List<Car> carList) {
+        RacingCarGameWinners racingCarGameWinners = new RacingCarGameWinners();
+        return racingCarGameWinners.getWinners(carList);
+    }
+}
